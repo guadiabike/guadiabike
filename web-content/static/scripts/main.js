2ae426da7305295509e59f9162ab310bcf27d96b
guadiabike <guadiabike.info@gmail.com>
2014-12-16 17:23:16 +0100

    Material design interfase

--- web-content/static/scripts/main.js
+++ web-content/static/scripts/main.js
@@ -1,12 +1,12 @@
 $(window).scroll(function() {
-   if($(window).scrollTop() - 40 > 0) {
-       $('#jp-main-header').css({'height': '30px'});
+   if($(window).scrollTop() - 35 > 0) {
+       $('#jp-main-header').css({'height': '30px'}).css({'box-shadow': '0px 2px 2px #888888'});
        $('#jp-main-header .content').css({'height': '30px'});
-       $('#jp-main-header .content .logo').css({'font-size': '15px'});
+       $('#jp-main-header .content .logo').css({'font-size': '1.4em'});
    }
    else {
-       $('#jp-main-header').css({'height': '70px'});
+       $('#jp-main-header').css({'height': '70px'}).css({'box-shadow': 'none'});
        $('#jp-main-header .content').css({'height': '70px'});
-       $('#jp-main-header .content .logo').css({'font-size': '32px'});
+       $('#jp-main-header .content .logo').css({'font-size': '2.5em'});
    }
 });
