c5cbbf21aa9315280316fd6c40ab5d0d96974638
guadiabike <guadiabike.info@gmail.com>
2014-12-17 19:13:41 +0100

    Material design interfase

--- web-content/static/scripts/main.js
+++ web-content/static/scripts/main.js
@@ -1,12 +1,15 @@
 $(window).scroll(function() {
-   if($(window).scrollTop() - 35 > 0) {
-       $('#jp-main-header').css({'height': '30px'}).css({'box-shadow': '0px 2px 2px #888888'});
-       $('#jp-main-header .content').css({'height': '30px'});
-       $('#jp-main-header .content .logo').css({'font-size': '1.4em'});
-   }
-   else {
-       $('#jp-main-header').css({'height': '70px'}).css({'box-shadow': 'none'});
-       $('#jp-main-header .content').css({'height': '70px'});
-       $('#jp-main-header .content .logo').css({'font-size': '2.5em'});
-   }
+
+    if($('#large-mode').is(':visible')) {
+        if ($(window).scrollTop() - 35 > 0) {
+            $('#jp-main-header').css({'height': '30px'}).css({'box-shadow': '0px 2px 2px #888888'});
+            $('#jp-main-header .content').css({'height': '30px'});
+            $('#jp-main-header .content .logo').css({'font-size': '1.4em'});
+        }
+        else {
+            $('#jp-main-header').css({'height': '70px'}).css({'box-shadow': 'none'});
+            $('#jp-main-header .content').css({'height': '70px'});
+            $('#jp-main-header .content .logo').css({'font-size': '2.5em'});
+        }
+    }
 });
